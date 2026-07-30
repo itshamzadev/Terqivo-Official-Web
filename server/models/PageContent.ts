dd83c4b953365ecdcd7bbc35efbb8ceb231e1b20
Its Hamza Dev <itshamzadev.com@gmail.com>
2026-07-30 05:33:54 +0500

    Reset repository

--- server/models/PageContent.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const pageContentSchema = new Schema({
-  page: { type: String, required: true, unique: true },
-  title: { type: String, required: true },
-  content: { type: Schema.Types.Mixed, required: true },
-}, { timestamps: true });
-
-export const PageContent = model('PageContent', pageContentSchema);
